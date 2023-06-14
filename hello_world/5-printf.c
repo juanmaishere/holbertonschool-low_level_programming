e17f802120b2e27eb10630aaf663d0317510b645
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-14 16:00:05 -0700

    v

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printf a string
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char str1[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf(str1);
+	return (0);
+}
