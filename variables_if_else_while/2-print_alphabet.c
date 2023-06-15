0e385864ec7051d020ecc37f22251a2ec9e9b7a3
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-15 15:32:59 -0700

    v

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - printf a string
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char c;
+		for (c = 'a'; c <= 'z'; ++c)
+			putchar(c);
+			putchar('\n');
+return (0);
+}
