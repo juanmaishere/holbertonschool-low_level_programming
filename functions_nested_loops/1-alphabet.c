9411e491ab9081b2f745d9b33a182d7604244612
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-16 10:53:29 -0700

    v

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - writes the character c to stdout
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int main(void)
+{
+	char an;
+
+	for (an = 'a'; an <= 'z'; an++)
+	{
+		_putchar(an);
+	}
+	_putchar('\n');
+	return (0);
+}
