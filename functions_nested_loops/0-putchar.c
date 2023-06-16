4a14eaf9b26e60a046e191814e8a398959646e1a
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-16 11:04:07 -0700

    v

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,6 @@
 #include "main.h"
-/**
- * main - writes the character c to stdout
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
+
+
 int main(void)
 {
 	_putchar('_');
--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
 * main - writes the character c to stdout
 *
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
+
 int main(void)
 {
 	char an;
