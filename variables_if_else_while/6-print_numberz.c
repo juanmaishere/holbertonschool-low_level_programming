d8544fb8a4cfdf1aca21c12bc212300d5af30b96
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-15 19:42:54 -0700

    well the same

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - printf a string
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+return (0);
+}
