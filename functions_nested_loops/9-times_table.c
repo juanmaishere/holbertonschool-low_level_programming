d3348998c20cb9788f671d03334229c5d5ded8c1
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-19 14:00:46 -0700

    franky d youngg

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+void
+times_table(void)
+{
+int a = 0;
+int b;
+int pep, reg, reg2;
+
+	for (a = 0; a <= 9; a++)
+		{
+		for (b = 0; b <	10; b++)
+		{
+		pep = b * a;
+		if (pep >= 10)
+			{
+			reg = pep / 10;
+			reg2 = pep % 10;
+			_putchar(reg + '0');
+			_putchar(reg2 + '0');
+			}
+		else
+		{
+		_putchar(' ');
+		_putchar(pep + '0');
+		}
+		if (b < 9)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+		}
+		_putchar('\n');
+}
+}
