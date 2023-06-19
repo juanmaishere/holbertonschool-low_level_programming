571ca1fce43572804d2f906e80f63263c5df2eed
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-19 09:13:21 -0700

    if else if

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*int print_sign(int n) - checks for a number , if it is positive negative or 0
+*@n: int n
+*Description: returns 1 or 0 depending on the if condition
+*Return: Always 0.
+*/
+int
+print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+		else if (n < 0)
+		{
+			_putchar('-');
+			return (-1);
+		}
+	else
+		{
+		return (0);
+		}
+}
