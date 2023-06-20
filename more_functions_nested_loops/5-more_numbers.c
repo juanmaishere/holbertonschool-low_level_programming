8e901a301b672d27c3eb4f1866614aa6cd6e108b
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-20 11:25:46 -0700

    3putchar

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,18 +11,14 @@ more_numbers(void)
 	for (b = 0; b <= 10; b++)
 {
 		for (a = 0; a <= 14; a++)
-		{
+		{	rei = a;
 			if (a >= 10)
 			{
 			rey = a / 10;
 			rei = a % 10;
 			_putchar(rey + '0');
-			_putchar(rei + '0');
 			}
-		else
-		{
-			_putchar(a + '0');
-		}
+			_putchar(rei + '0');
 		}
 		_putchar('\n');
 }
