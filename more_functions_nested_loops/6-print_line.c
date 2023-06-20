ace7f45b463eee90627eeeb5b4ab8ca7cca88838
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-20 11:44:37 -0700

    PUSHER

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+void
+print_line(int n)
+{
+	int b;
+
+	b = 0;
+	if (n > 0)
+	{
+	do {
+	_putchar('_');
+		b++;
+	} while (b <= n);
+	_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+
+
+
+}
+
