ffeb44901a1610ad35b1aa2d7d5314facf94b28f
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-20 19:24:49 -0700

    fixed

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+void
+print_diagonal(int n)
+{
+	int spacio;
+	int barra;
+
+if (n > 0)
+{
+	for (barra = 0; barra < n; barra++)
+	{
+		for (spacio = 0; spacio < barra; spacio++)
+		{
+		putchar(' ');
+		}
+	putchar('\\');
+	putchar('\n');
+	}
+}
+}
