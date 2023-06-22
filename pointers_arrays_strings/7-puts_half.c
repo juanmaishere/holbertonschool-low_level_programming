bc9d3a9ef4932257c536026cbd997073bc47fe7f
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-22 13:08:08 -0700

    2FOR2IF

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+void
+puts_half(char *str)
+{
+int i, l, p;
+
+	l = 0;
+
+	for (p = 0; str[p] != '\0'; p++)
+	{
+		l++;
+	}
+
+	if (l % 2 == 0)
+	{
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i >= l / 2)
+		{
+			_putchar(str[i]);
+		}
+	}
+	}
+	else
+	{
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i >= (l - 1) / 2)
+		{
+			_putchar(str[i]);
+		}
+	}
+	}
+	_putchar('\n');
+}
