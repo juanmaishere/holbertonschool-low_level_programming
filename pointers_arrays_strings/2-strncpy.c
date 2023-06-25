538f406d8bb4e9538de27e5b0407b742d22438a3
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-25 08:16:48 -0700

    why

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+char
+*_strncpy(char *dest, char *src, int n)
+{
+	char *word = dest;
+
+	while (*word != '\0')
+	{
+
+	word++;
+	}
+/* word recorre la string de *dest y ahora igualamos a src, añado la string*/
+		while (*src != '\0' && n > 0)
+		{
+		*word = *src;
+		src++;
+		word++;
+		n--;
+		}
+	*word = '\0';
+return (dest);
+}
