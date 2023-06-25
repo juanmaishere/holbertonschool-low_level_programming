f8494d73ec7bfb8a9f1b94efbe39d427673b555f
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-25 16:38:57 -0700

    easy

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+char
+*cap_string(char *z)
+{
+	int i;
+	char *r = z;
+	int p = 0;
+
+	for (i = 0; z[i] != '\0'; i++)
+	{
+
+		if (z[i] >= 65 && z[i] <= 90)
+		{
+		p = 0;
+		}
+
+		if (p == 1)
+		{
+			if (z[i] >= 97 && z[i] <= 122)
+			{
+				r[i] = z[i] - 32;
+			p = 0;
+			}
+		}
+
+
+
+	if (p == 0)
+	{
+		if (z[i] >= 0 && z[i] <= 64)
+		{
+			p = 1;
+
+		}
+	}
+	}
+return (r);
+}
