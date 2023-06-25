5f537bc5b82a8de7b4dea194aeb013bc1fc8517f
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-25 07:15:33 -0700

    recorrerstringsdeus

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -5,25 +5,26 @@ _atoi(char *s)
 {
 int r;
 int si = 1;
-int rel = 0;
 int nb = 0;
+int be = 0;
 
 for (r = 0; s[r] != '\0'; r++)
 {
 	if (s[r] >= '0' && s[r] <= '9')
 	{
-		rel = (rel * 10) + (s[r] + '0');
+		nb = (s[r] * 10);
+		r++;
+		be = nb + (s[r] - '0');
 	}
 
 	else if (s[r] == '-')
 	{
 		si *= -1;
 	}
+	nb *= si;
 
-	nb = (rel * si);
-	break;
 }
 
 
-return (nb);
+return (be);
 }
