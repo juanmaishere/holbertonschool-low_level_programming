42f0be3c8f30e3dd58e0d6b22089016a7d9d5bc2
juanmaishere <rsjuanmanuel@hotmail.com>
2023-06-27 15:37:18 -0700

    this was actually really fun

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,12 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 void
 print_chessboard(char (*a)[8])
 {
 	int i;
+	int b;
+	char el;
 
-	for (i = 0; a[i] != a[8]; i++)
+	for (i = 0; i < 8; i++)
 	{
-	_putchar(a);
+		for (b = 0; b < 8; b++)
+		{
+		el = a[i][b];
+		printf("%c", el);
+		}
+		printf("\n");
 	}
 }
