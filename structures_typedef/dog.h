8983c96b40e7aff007ebff8f47438310288fcd57
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-10 07:30:25 -0700

    dog struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,8 @@
-#include <stdio.h>
+
 #ifndef dog_h
 #define dog_h
 /**
-struct my_struct - Short description
+ *struct dog - Short description
  *@name: First member
  *@age: Second member
  *@owner: Third member
@@ -17,20 +17,20 @@ struct dog
 	 *		 *
 	 *			 * Description: You can use paragraphs to describe arguments
 	 *				 * using this method.
-	 *					 */
+	 */
 	float age;
 	/**
 	 *	 * @age: This is longer description of C
 	 *		 *
 	 *			 * Description: You can use paragraphs to describe arguments
 	 *				 * using this method.
-	 *					 */
+	 */
 	char *owner;
 	/**
 	 *	 * @owner: This is longer description of C
 	 *		 *
 	 *			 * Description: You can use paragraphs to describe arguments
 	 *				 * using this method.
-	 *					 */
-}dog;
+	 */
+} dog;
 #endif
