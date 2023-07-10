6254d6c2750a1f43e22982f7dd8d8262049f9f3b
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-10 07:08:53 -0700

    struct type doggo

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef dog
+/**
+struct my_struct - Short description
+ * @name: First member
+ *  * @age: Second member
+ *   * @owner: Third member
+ *    *
+ *Description: DOGGO STRUCTURE DATA BEING GOOOD:
+ */
 struct dog
 {
 	char* name;
 	float age;
 	char* owner;
 };
+#endif
