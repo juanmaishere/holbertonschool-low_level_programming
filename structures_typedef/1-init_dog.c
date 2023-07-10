32a4e63d084762f5953a494696dd13d694b73449
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-10 08:31:23 -0700

    typer

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,16 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 void
 init_dog(struct dog *d, char *name, float age, char *owner)
 {
 struct dog *dog;
 
-	dog = d;
+dog = malloc(sizeof(struct dog));
+	if (d == (NULL))
+		return;
+dog = d;
 
 dog->name = name;
 dog->age = age;
