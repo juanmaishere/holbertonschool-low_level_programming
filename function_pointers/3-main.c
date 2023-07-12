cc977540fd45631b2c4c778fd748b24ff976569a
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-12 05:42:05 -0700

    d

---
+++ function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char *operador;
+
+	if (argc != 4)
+		{
+		printf("Error\n");
+		exit(98);
+		}
+
+
+	num1 = atoi(argv[1]);
+	operador = argv[2];
+	num2 = atoi(argv[3]);
+
+	result = get_op_func(operador)(num1, num2);
+	printf("%d", result);
+	printf("\n");
+return (0);
+}
