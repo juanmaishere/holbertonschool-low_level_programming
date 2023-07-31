e4762875f475cfd675b7421c9d22732ea19befbf
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-30 18:45:18 -0700

    feat

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int _putchar(int c);
 unsigned int binary_to_uint(const char *b);
 void reverser(char *str);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
