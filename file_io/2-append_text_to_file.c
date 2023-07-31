9ab7fdbda0d89b872d49ef36711f45f74e694535
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-31 13:09:15 -0700

    feat

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-int append_text_to_file(const char *filename, char *text_content)
+int
+append_text_to_file(const char *filename, char *text_content)
 {
 int d;
 unsigned long int c;
--- file_io/main.h
+++ file_io/main.h
@@ -1,3 +1,5 @@
+#define HEADER_h
+#ifndef HEADER_h
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -7,3 +9,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+#endif
