f377143c45b927290e7124dff6817e1150de465f
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-31 13:06:10 -0700

    feat

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int d;
+unsigned long int c;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	d = open(filename, O_WRONLY | O_APPEND);
+
+	if (d == -1)
+		return (-1);
+
+	if (text_content == NULL)
+	{
+	close(d);
+	}
+	else
+	{
+	c = write(d, text_content, strlen(text_content));
+
+	if (c != strlen(text_content))
+		return (-1);
+
+	close(d);
+	}
+
+return (1);
+}
