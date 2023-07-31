297d26f00e3bb13792cfb81f52ac39a8864e54f2
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-31 13:11:36 -0700

    feat

--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,3 @@
-#define HEADER_h
-#ifndef HEADER_h
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -9,4 +7,3 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-#endif
