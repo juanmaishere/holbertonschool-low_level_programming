0f0ca429d66af1cdc47493ec84abe38232bf5625
juanmaishere <rsjuanmanuel@hotmail.com>
2023-07-31 09:46:43 -0700

    fix1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,12 +13,18 @@ create_file(const char *filename, char *text_content)
 
 	if (d != -1)
 	{
+	if (text_content != NULL)
+	{
 	write(d, text_content, strlen(text_content));
+	}
 	close(d);
 	}
 	else
 	{
+	if (text_content != NULL)
+	{
 	write(d, text_content, strlen(text_content));
+	}
 	chmod(filename, 0600);
 	close(d);
 	}
