58106e2dd62ee9e35e0bef6500921d4a1526acf0
juanmaishere <rsjuanmanuel@hotmail.com>
2023-08-01 13:11:04 -0700

    feat

---
+++ file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#define BUFFER_SIZE 4096
+int main(int argc, char *argv[])
+{	int writen, fd = 0, fd2 = 0;
+	char *from, *to, bytes;
+	char buffer[10020];
+
+	from = argv[1];
+	to = argv[2];
+
+	if (argc != 3)
+	{
+	printf("Usage: cp file_from file_to\n");
+	exit(97);
+	}
+
+	fd = open(from, O_RDONLY);
+	fd2 = open(to, O_WRONLY | O_CREAT | O_TRUNC);
+	if (fd2 == -1)
+	{
+	chmod(argv[2], 664);
+	}
+	bytes = read(fd, buffer, BUFFER_SIZE);
+	if (bytes == -1)
+	{
+	printf("Error: Can't read from file %s\n", argv[1]);
+	exit(98);
+	}
+	writen = write(fd2, buffer, bytes);
+	if (writen == -1)
+	{
+	printf("Error: Can't write to %s\n", argv[2]);
+	exit(99);
+	}
+	close(fd2);
+	close(fd);
+	if (close(fd) == -1)
+	{
+	exit(100); }
+	if (close(fd2) == -1)
+	{
+	exit(100); }
+return (writen);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -1,3 +1,5 @@
+#ifndef HEADER_H
+#define HEADER_H
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
@@ -7,3 +9,5 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+#endif
