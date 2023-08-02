a1b2ae281aeafeae4a47302db26531fd2dea9026
juanmaishere <rsjuanmanuel@hotmail.com>
2023-08-02 08:20:34 -0700

    feat

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,7 +10,10 @@ create_file(const char *filename, char *text_content)
 	}
 
 	d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-
+	if (d == -1)
+	{
+	return (-1);
+	}
 	if (d != -1)
 	{
 	if (text_content != NULL)
