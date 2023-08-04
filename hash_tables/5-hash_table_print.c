dfe12a48789e6aff1a1efc4e020a160fa8a037a5
juanmaishere <rsjuanmanuel@hotmail.com>
2023-08-03 19:09:34 -0700

    faet

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+void
+hash_table_print(const hash_table_t *ht)
+{
+	int i = 0;
+
+	hash_node_t *node;
+
+	printf("{");
+	while (i < ht->size)
+	{
+	node = ht->array[i];
+	while (node != NULL)
+	{
+	printf("'%s': %s", node->key, node->value);
+	node = node->next;
+	if (node != NULL)
+		{
+		printf(", ");
+		}
+	}
+	i++;
+	}
+	printf("{\n");
+}
