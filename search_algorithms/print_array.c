12423f5979d69815ea47df54de1bc8f4423a0507
juanmaishere <rsjuanmanuel@hotmail.com>
2023-09-18 00:05:01 +0000

    feat

--- search_algorithms/print_array.c
+++ search_algorithms/print_array.c
@@ -8,7 +8,8 @@
  * @array: The array to be printed
  * @size: Number of elements in @array
  */
-void print_array(const int *array, size_t size)
+void
+print_array(const int *array, size_t size)
 {
     size_t i;
 
